915cba4b29b676a405ad1c7ca2fa30f890fdbb46
HudChew <hudchew@gmail.com>
2018-09-26 17:39:49 +0700

    -

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAlTw4Bh7vIvaj6MCNKie6r-qEPFUkj0jg",
-    authDomain: "hudchew-65be3.firebaseapp.com",
-    databaseURL: "https://hudchew-65be3.firebaseio.com",
-    projectId: "hudchew-65be3",
-    storageBucket: "hudchew-65be3.appspot.com",
-    messagingSenderId: "620449897795"
-}
